8e1ee23009821900367b2212c5e5dec5cba462d6
Miran Jang <miranjang9507@gmail.com>
2019-07-28 19:34:59 -0400

    progressing

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+$(document).ready(function (){
+
+
+// make 8 questions
+var questionObj = {
+    q1: {
+        question: "Which is not a Game of Thrones royal family name?",
+        answer: ["Stark", "Arryn", "Martell", "Baelish"]
+    },
+
+    q2: {
+        question: "What's the name of Arya Stark's sword?",
+        answer: ["Long Claw", "Oath Keeper", "Needle", "Heartsbane"]
+    },
+
+    q3: {
+        question: "Which character is the Three-Eyed Raven?",
+        answer: ["The High Sparrow", "Bran Stark", "Jaqen H'ghar", "Darrio Naharis"]
+    },
+
+    q4: {
+        question: "Which one is the Tyrell’s ancestral home?",
+        answer: ["Winterfell", "Highgarden", "The Vale", "Stormlands"]
+    },
+
+    q5: {
+        question: "Aside from Valyrian Steel, what else can kill White Walkers?",
+        answer: ["A raven", "Dragonglass", "Greyscale", "Ice"]
+    },
+
+    q6: {
+        question: "Who actually poisoned Joffrey?",
+        answer: ["Sansa Stark", "Tyrion Lannister", "Olenna Tyrell", "Lord Varys"]
+    },
+
+    q7: {
+        question: "By which of these names is she not known?",
+        answer: ["Mother of Dragon", "Mhysa", "White Queen", "Khaleesi"]
+    },
+
+    q8: {
+        question: "How are Daenerys and Jon Snow related?",
+        answer: ["She's his cousin", "She's his aunt", "She's his half-sister", "She's his real mother"]
+    }
+}
+
+console.log(questionObj.q1.question);
+console.log(questionObj.q2.answer);
+
+var correctAnswer = 0;
+var wrongAnswer = 0;
+var unAnswer = 0;
+
+
+
+
+
+
+
+});
