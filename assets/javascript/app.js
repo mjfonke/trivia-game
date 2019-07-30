1587195ba0f1c93d0689a523373138386d057bce
Miran Jang <miranjang9507@gmail.com>
2019-07-30 09:56:10 -0400

    working on it..

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,58 +2,181 @@ $(document).ready(function (){
 
 
 // make 8 questions
-var questionObj = {
-    q1: {
+var questionObj = [
+    {
         question: "Which is not a Game of Thrones royal family name?",
-        answer: ["Stark", "Arryn", "Martell", "Baelish"]
+        choice: ["Stark", "Arryn", "Martell", "Baelish"],
+        rightAnswer: 3,
+        answer: "Baelish",
+        gif: "../images/littlefinger"
+        
     },
 
-    q2: {
+    {
         question: "What's the name of Arya Stark's sword?",
-        answer: ["Long Claw", "Oath Keeper", "Needle", "Heartsbane"]
+        choice: ["Long Claw", "Oath Keeper", "Needle", "Heartsbane"],
+        rightAnswer: 2,
+        answer: "Needle",
+        gif: "../images/needle"
+
     },
 
-    q3: {
+    {
         question: "Which character is the Three-Eyed Raven?",
-        answer: ["The High Sparrow", "Bran Stark", "Jaqen H'ghar", "Darrio Naharis"]
+        choice: ["The High Sparrow", "Bran Stark", "Jaqen H'ghar", "Darrio Naharis"],
+        rightAnswer: 1,
+        answer: "Bran Stark",
+        gif: "../images/bran"
     },
 
-    q4: {
+    {
         question: "Which one is the Tyrell’s ancestral home?",
-        answer: ["Winterfell", "Highgarden", "The Vale", "Stormlands"]
+        choice: ["Winterfell", "Highgarden", "The Vale", "Stormlands"],
+        rightAnswer: 1,
+        answer: "Highgarden",
+        gif: "../images/highgarden"
     },
 
-    q5: {
+    {
         question: "Aside from Valyrian Steel, what else can kill White Walkers?",
-        answer: ["A raven", "Dragonglass", "Greyscale", "Ice"]
+        choice: ["A raven", "Dragon Glass", "Greyscale", "Ice"],
+        rightAnswer: 1,
+        answer: "Dragon Glass",
+        gif: "../images/nightking"
     },
 
-    q6: {
+    {
         question: "Who actually poisoned Joffrey?",
-        answer: ["Sansa Stark", "Tyrion Lannister", "Olenna Tyrell", "Lord Varys"]
+        choice: ["Sansa Stark", "Tyrion Lannister", "Olenna Tyrell", "Lord Varys"],
+        rightAnswer: 2,
+        answer: "Olenna Tyrell",
+        gif: "../images/joffrey"
     },
 
-    q7: {
-        question: "By which of these names is she not known?",
-        answer: ["Mother of Dragon", "Mhysa", "White Queen", "Khaleesi"]
+    {
+        question: "By which of these names is Daenerys not known?",
+        choice: ["Mother of Dragon", "Mhysa", "White Queen", "Khaleesi"],
+        rightAnswer: 2,
+        answer: "White Queen",
+        gif: "../images/dany"
+
     },
 
-    q8: {
+    {
         question: "How are Daenerys and Jon Snow related?",
-        answer: ["She's his cousin", "She's his aunt", "She's his half-sister", "She's his real mother"]
+        choice: ["She's his cousin", "She's his aunt", "She's his half-sister", "She's his real mother"],
+        rightAnswer: 1,
+        answer: "She's his aunt",
+        gif: "../images/jon"
     }
-}
+]
 
-console.log(questionObj.q1.question);
-console.log(questionObj.q2.answer);
+
 
 var correctAnswer = 0;
 var wrongAnswer = 0;
 var unAnswer = 0;
+var showQuestions;
+var count = 0;
+var timer = 20;
+var intervalId;
+var respond = false;
+var userAnswer;
+
+
+$("#start").click(gameStart);
+
+
+
+function gameRunning(){
+
+    timerRunning();
+    decrement();
+
+    $("#questions").html("<h2"> + questionObj[count].question + "</h2>");
+    
+    for (var i = 0; i <4; i++) {
+        var multipleChoice = $("<button>");
+        multipleChoice.text(questionObj[count].choice[i]);
+        multipleChoice.addClass("mc");
+        multipleChoice.attr("data-choicevalue", i);
+        $("#questions").append(multipleChoice);
+    }
+
+    $(".mc").on("click", function (){
+        userAnswer = $(this).data("index");
+        clearInterval(intervalId);
+        respond = true;
+        feedback();
+    })
+}
+
+
+
+function gameStart() {
+    $("#start").hide;
+    correctAnswer = 0;
+    wrongAnswer = 0;
+    unAnswer = 0;
+    gameRunning();
+    console.log("running")
+}
+
+
+
+function timerRunning() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    timer--;
+    
+    $("#timer-text").html("<h2>" + "Time Remaining: " + timer + "</h2>");
+
+    if (timer === 0) {
+
+        clearInterval(intervalId);
+
+        respond = false;
+
+        $("#timer-text").html("<h2>" + "Times up!!" + "</h2>");
+        
+        feedback();
+
+        
+    }
+}
 
 
 
 
+
+function feedback () {
+    
+    count++;
+    
+    var imageHolder = $("<img>");
+    imageHolder.addClass("gifImage");
+    imageHolder.attr("src", questionObj[count].gif);
+    $("#questions").append(imageHolder);
+
+    if (userAnswer === questionObj[count].rightAnswer && respond === true) {
+        correctAnswer++;
+        $("#feedback-text").html("You're right!! The answer was " + questionObj[count].answer)
+    }
+
+    else if (userAnswer != questionObj[count].rightAnswer && respond === true) {
+        wroingAnswer++;
+        $("#feedback-text").html("Incorrect!! The answer was " + questionObj[count].answer);
+    }
+
+    else {
+        unAnswer++;
+        $("#feedback-text").html("Out of time!! The answer was " + questionObj[count].answer)
+    }
+
+}
 
 
 
